f70daa8b3adcc521193ff24367235e6c34272a14
santosh <santosh.ece06@gmail.com>
2018-10-07 16:34:55 +0530

    Primary toolbar.

--- src/app/route.module.ts
+++ src/app/route.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
-import {FlexLayoutModule} from '@angular/flex-layout';
 import { Page3Component } from './page3/page3.component';
 
 const appRoutes: Routes = [
@@ -20,7 +22,8 @@ const appRoutes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forRoot(appRoutes),
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatToolbarModule
   ],
   exports: [RouterModule],
   declarations: [HomeComponent, Page1Component, Page2Component, Page3Component]
