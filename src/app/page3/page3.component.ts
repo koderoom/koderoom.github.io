1f1212465db5c124675bf4d795384b92785adc0f
santosh <santosh.ece06@gmail.com>
2018-10-07 12:55:24 +0530

    page 3 sample

--- src/app/page3/page3.component.ts
+++ src/app/page3/page3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ObservableMedia} from '@angular/flex-layout';
 
 @Component({
   selector: 'app-page3',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page3.component.css']
 })
 export class Page3Component implements OnInit {
+  defaultbg: any = 'mediumslateblue';
+  appbg: any = {};
 
-  constructor() { }
+  constructor(public media: ObservableMedia ) { }
 
   ngOnInit() {
+    this.media.subscribe((data) => {
+      if (data.mqAlias === 'xs') {
+        this.appbg.background = 'gold';
+      } else if (data.mqAlias === 'sm') {
+        this.appbg.background = 'chocolate';
+      } else if (data.mqAlias === 'md') {
+        this.appbg.background = '#009688';
+      }  else {
+        this.appbg.background = this.defaultbg;
+      }
+    });
   }
 
 }
