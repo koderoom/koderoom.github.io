aefa1c9fdb1ffef29aab720e2522a9bfe8e9372d
santosh <santosh.ece06@gmail.com>
2018-10-07 17:37:51 +0530

    mat-card added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 
 import {RouteModule, RoutingComponents} from './route.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FlexLayoutModule,
     MatToolbarModule,
     MatCardModule,
+    MatButtonModule,
     RouteModule,
   ],
   providers: [],
