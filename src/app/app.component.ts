1e978f92af197f7351ee3d6e1452634d5ab868bb
santosh <santosh.ece06@gmail.com>
2018-10-14 17:19:58 +0530

    theme.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,10 @@ export class AppComponent {
   title = 'koderoom-home';
 
   appThemeList = [
-        {'name' : AppThemes.CUSTOM_INDIGO_PINK, 'color': '#3F51B5'},
-        {'name' : AppThemes.CUSTOM_DEEP_PURPLE, 'color': '#673AB7'},
-        {'name' : AppThemes.CUSTOM_PINK_BLUE_GREY, 'color': '#F44336'},
-        {'name' : AppThemes.CUSTOM_PURPLE_GREEN, 'color': '#9C27B0'},
+        {'name' : AppThemes.CUSTOM_INDIGO_PINK, 'color': '#3F51B5', 'title' : 'Indigo Blue'},
+        {'name' : AppThemes.CUSTOM_DEEP_PURPLE, 'color': '#673AB7', 'title' : 'Deep Purple'},
+        {'name' : AppThemes.CUSTOM_PINK_BLUE_GREY, 'color': '#F44336', 'title' : 'Pink Grey'},
+        {'name' : AppThemes.CUSTOM_PURPLE_GREEN, 'color': '#9C27B0', 'title': 'Purple Green'},
   ];
   appTheme = AppThemes.CUSTOM_INDIGO_PINK;
 
