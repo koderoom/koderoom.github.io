419acd8e730e750d7c138c544a5425f277454557
santosh <santosh.ece06@gmail.com>
2018-10-14 14:10:59 +0530

    themeing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,15 @@ import { AppThemes } from 'src/app/common/app-constants.enum';
 export class AppComponent {
   title = 'koderoom-home';
 
-  appThemeList = [AppThemes.CUSTOM_INDIGO_PINK, AppThemes.CUSTOM_DEEP_PURPLE,
-                  AppThemes.CUSTOM_PINK_BLUE_GREY, AppThemes.CUSTOM_PURPLE_GREEN];
+  appThemeList = [
+        {'name' : AppThemes.CUSTOM_INDIGO_PINK, 'color': '#3F51B5'},
+        {'name' : AppThemes.CUSTOM_DEEP_PURPLE, 'color': '#673AB7'},
+        {'name' : AppThemes.CUSTOM_PINK_BLUE_GREY, 'color': '#F44336'},
+        {'name' : AppThemes.CUSTOM_PURPLE_GREEN, 'color': '#9C27B0'},
+  ];
   appTheme = AppThemes.CUSTOM_INDIGO_PINK;
 
-  changeTheme(selectedTheme) {
-    this.appTheme = selectedTheme;
+  changeTheme(item) {
+    this.appTheme = item.name;
   }
 }
